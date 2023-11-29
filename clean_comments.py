ce5470c08cf0dbb2a2eeda7af5d0d34ad1fd30ff
Dmitry Mikushin <dmitry@kernelgen.org>
2023-11-30 00:51:32 +0100

    Adding a script to clean some comments

---
+++ clean_comments.py
@@ -0,0 +1,34 @@
+import re
+import sys
+
+comment1 = r"""!
+!.*argument declarations
+!
+"""
+
+comment2 = r"""!
+!.*variable declarations.*
+!
+"""
+
+comment3 = r"""!
+! End of declarations.*
+!
+"""
+
+# Check if the file path is provided as a command-line argument
+if len(sys.argv) < 2:
+    print("Please provide the file path as a command-line argument.")
+    sys.exit(1)
+
+file_path = sys.argv[1]
+
+with open(file_path, 'r') as file:
+    code = ''.join(file.readlines())
+
+code = re.sub(comment1, '', code)
+code = re.sub(comment2, '', code)
+code = re.sub(comment3, '', code)
+
+with open(file_path, 'w') as file:
+    file.write(code)
