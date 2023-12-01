dddaa2e8d8ed19c383f9d7dd6b1404fdf71fc7dc
Dmitry Mikushin <dmitry@kernelgen.org>
2023-12-01 18:27:54 +0100

    Adding compilation script, adding mouse-based focus into GNU screen

--- process_modules.py
+++ process_modules.py
@@ -46,6 +46,10 @@ for module_name in modules.keys():
         function_names = list(modules[module_name].values())[0]
         for function_name in function_names:
             library = re.sub('/[^\/]+', '/', function_name)
+            # TODO Validate by compiling the source folder with a Makefile
+            # TODO Do not remove - copy the module, so that the source could still be compiled!
+            # TODO Copy the sources as well
+            # TODO Remove the source folder, only if all modules for it are a single representation
             os.system(f'mv {folder_path}/{function_name}/c_{module_name}.f90 {library}')
             os.system(f'git rm {folder_path}/{function_name}/c_{module_name}.f90')
     elif len(modules[module_name].keys()) == 2:
@@ -58,22 +62,32 @@ for module_name in modules.keys():
         module_file1 = f'{folder_path}/{function_names[0][0]}/c_{module_name}.f90'
         module_file2 = f'{folder_path}/{function_names[1][0]}/c_{module_name}.f90'
         
-        # TODO Create a temp folder with symbolic links to all sources,
+        # Create a temp folder with symbolic links to all sources,
         # prefixed with the index of representation.
+        # Create a compile.sh script to validate the compilation of
+        # all sources.
         os.system('rm -rf ./process_modules_temp')
         os.system('mkdir ./process_modules_temp')
-        for function_name in function_names[0]:
-            linkname = re.sub('\/', '_', function_name)
-            filename = re.sub('.*\/', '', function_name)
-            os.system(f'ln -s ../{folder_path}/{function_name}/{filename}.f90 ./process_modules_temp/{linkname}.f90')
-        for function_name in function_names[1]:
-            linkname = re.sub('\/', '_', function_name)
-            filename = re.sub('.*\/', '', function_name)
-            os.system(f'ln -s ../{folder_path}/{function_name}/{filename}.f90 ./process_modules_temp/{linkname}.f90')
+        compile_sh = '#!/bin/bash\n'
+        compile_sh += 'set +e\n'
+        for function_names in modules[module_name].values():
+            for function_name in function_names:
+                linkname = re.sub('\/', '_', function_name)
+                filename = re.sub('.*\/', '', function_name)
+                os.system(f'ln -s ../{folder_path}/{function_name}/{filename}.f90 ./process_modules_temp/{linkname}.f90')
+                compile_sh += 'gfortran -Wall -J../include '
+                compile_sh += f'../{folder_path}/{function_name}/{filename}/c_*.f90 '
+                compile_sh += f'../{folder_path}/{function_name}/{filename}.f90 '
+                compile_sh += f'-o lib{filename}.so\n'
+        with open('./process_modules_temp/compile.sh', "w") as screenrc_file:
+            screenrc_file.write(compile_sh)
+        os.system('chmod +x ./process_modules_temp/compile.sh')
         
         # Create screen or tmux with two modules split on the left and
         # "mc" browsing the sources links folder on the right.
         screenrc = f"""
+            mousetrack on
+            defmousetrack on
             screen -t name1 vim {module_file1}
             split -v
             focus
@@ -88,10 +102,17 @@ for module_name in modules.keys():
         """
         with open('./process_modules_temp/.screenrc', "w") as screenrc_file:
             screenrc_file.write(screenrc)
+        # Emergency stop in case of a bug:
+        # killall -9 python3 && killall -9 screen
         os.system(f'screen -c ./process_modules_temp/.screenrc')
 
         # TODO Can we get a special key shortcut to quickly switch
         # between items?
+        # We need the following keys:
+        # NEXT problem
+        # PREV problem
+        # STOP and exit
+        # SUBMIT if validation passes for all concerned sources, release the module ONLY COPY (+copy the changed sources) and git-push the change
         two_representations += 1
     for function_names in modules[module_name].values():
         print('\t' + ', '.join(function_names))
