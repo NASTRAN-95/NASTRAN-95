3ab35ef88404263de84a61261dfb974a59b004c7
Dmitry Mikushin <dmitry@kernelgen.org>
2023-12-03 00:18:19 +0100

    Automatically merging DATA initializations from BLOCKDATA to the MODULE themselves, for BLOCKDATA sources that reference only a single MODULE use

--- source/bd/flbbd/c_flbfil.f90
+++ source/bd/flbbd/c_flbfil.f90
@@ -2,4 +2,23 @@
 MODULE C_FLBFIL
    INTEGER :: Af, Afdict, Afmat, Bgpdt, Conect, Cstm, Dkgg, Ect, Eqexin, Fbelm, Frelm, Geom2, Geom3, Kgdict, Kgmat,  &
             & Mpt, Sil, Uset, Usetf, Usets
-END MODULE C_FLBFIL
+!FLBBD
+!     FLBBD - BLOCK DATA FOR MODULE FLBMG
+!
+!
+!     GINO FILES
+!
+!
+!     INPUT DATA BLOCKS
+!
+   DATA geom2 , ect , bgpdt , sil , mpt , geom3 , cstm , uset , eqexin/101 , 102 , 103 , 104 , 105 , 106 , 107 , 108 , 109/
+!
+!     OUTPUT DATA BLOCKS
+!
+   DATA usetf , usets , af , dkgg/201 , 202 , 203 , 204/
+!
+!     INTERNAL SCRATCH FILES
+!
+   DATA fbelm , frelm , conect , afmat , afdict , kgmat , kgdict/301 , 302 , 303 , 304 , 305 , 306 , 307/
+!
+END MODULE c_flbfil
--- source/bd/vdrbd/c_vdrcom.f90
+++ source/bd/vdrbd/c_vdrcom.f90
@@ -7,4 +7,27 @@ MODULE C_VDRCOM
    INTEGER, DIMENSION(2) :: Cei, Direct, Frq, Modal, Nam, Trn
    INTEGER, DIMENSION(6) :: Masks
    REAL :: Vdrcom, Vdrreq
-END MODULE C_VDRCOM
+!VDRBD
+! BLOCK DATA FOR THE VECTOR DATA RECOVERY MODULE (VDR).
+!*****
+!
+!
+!
+! DATA DEFINING POSITION OF PARAMETERS IN CASE CONTROL RECORD.
+!
+   DATA idisp/20/ , ivel/32/ , iacc/29/ , ispcf/35/ , iloads/17/ , istr/23/ , ielf/26/ , iadisp/151/ , iavel/154/ , iaacc/157/ ,    &
+      & ipnl/10/ , ittl/39/ , ilsym/200/ , ifrout/145/ , idload/13/
+!
+! DATA DEFINING GINO FILE NAMES
+!
+   DATA casecc/101/ , eqdyn/102/ , usetd/103/ , infile/104/ , oeigs/105/ , pp/105/ , xycdb/106/ , pnl/107/ , outfle/201/ ,          &
+      & opnl1/202/ , scr1/301/ , scr2/302/
+!
+! MISC DATA
+!
+   DATA buf/50*0/ , nam/4HVDR  , 4H    / , masks/4 , 8 , 16 , 32 , 64 , 128/ , xset0/100000000/
+!
+! DATA DEFINING RIGID FORMATS AND PROBLEM TYPES
+!
+   DATA cei/4HCEIG , 4HEN  / , frq/4HFREQ , 4HRESP/ , trn/4HTRAN , 4HRESP/ , modal/4HMODA , 4HL   / , direct/4HDIRE , 4HCT  /
+END MODULE c_vdrcom
