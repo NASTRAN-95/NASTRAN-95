8a4cb7b0cd7c8a48cb896c6606bfd53cf144c794
Dmitry Mikushin <dmitry@kernelgen.org>
2023-12-03 00:28:28 +0100

    Merging DATA initializations from BLOCKDATA to the MODULE themselves for sma2bd

--- source/bd/sma2bd/c_sma2cl.f90
+++ source/bd/sma2bd/c_sma2cl.f90
@@ -4,4 +4,19 @@ MODULE C_SMA2CL
    REAL, DIMENSION(202) :: Dummy
    INTEGER :: Iopt4, Jmax, Left, Lrowic, Nlinks, Nogo, Npvt, Nrowsc
    INTEGER, DIMENSION(10) :: Link
+!SMA2BD
+!
+!
+!     SMA2 VARIABLE CORE BOOKKEEPING PARAMETERS
+!
+!
+!     SMA2 PROGRAM CONTROL PARAMETERS
+!
+!
+!     ECPT COMMON BLOCK
+!
+!
+   DATA nlinks/10/
+   DATA nogo/0/
+
 END MODULE C_SMA2CL
--- source/bd/sma2bd/c_sma2io.f90
+++ source/bd/sma2bd/c_sma2io.f90
@@ -3,4 +3,20 @@ MODULE C_SMA2IO
    INTEGER :: Clsnrw, Clsrw, Eor, Idum1, Idum2, Idum3, Idum4, Idum5, Ifbgg, Ifcstm, Ifdit, Ifecpt, Ifgpct, Ifmgg, Ifmpt,&
             & Igbgg, Igecpt, Iggpct, Igmgg, Inrw, Neor, Outrw
    INTEGER, DIMENSION(7) :: Mcbbgg, Mcbmgg
+!SMA2BD
+!
+!
+!     SMA2 VARIABLE CORE BOOKKEEPING PARAMETERS
+!
+!
+!     SMA2 PROGRAM CONTROL PARAMETERS
+!
+!
+!     ECPT COMMON BLOCK
+!
+!
+   DATA ifcstm , ifmpt , ifecpt , ifgpct , ifdit/101 , 102 , 103 , 104 , 105/
+   DATA ifmgg , ifbgg/201 , 202/
+   DATA inrw , clsrw , clsnrw , eor , neor , outrw/0 , 1 , 2 , 1 , 0 , 1/
+
 END MODULE C_SMA2IO
