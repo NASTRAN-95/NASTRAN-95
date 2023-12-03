f25a8c9ce3e152d6e5f36332ee2a85f54b1c8db7
Dmitry Mikushin <dmitry@kernelgen.org>
2023-12-03 21:16:58 +0100

    Unifying more modules with the requirements of their other users

--- source/mds/bunpak/c_bandb.f90
+++
@@ -1,5 +0,0 @@
-!*==/home/marcusmae/nasa/nastran/SPAGged/C_BANDB.f90  created by SPAG 8.01RF at 14:46 on  2 Dec 2023
-MODULE C_BANDB
-   INTEGER, DIMENSION(3) :: Dum3b
-   INTEGER :: Ipass, Nbit, Nw
-END MODULE C_BANDB
