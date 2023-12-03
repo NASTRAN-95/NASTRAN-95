4ee6992190b9a18a5eb48f414cdb942e1e8ef9cf
Dmitry Mikushin <dmitry@kernelgen.org>
2023-12-03 03:02:56 +0100

    Merging DATA initializations from BLOCKDATA to the MODULE themselves for sma1bd

--- source/bd/sma1bd/c_sma1cl.f90
+++ source/bd/sma1bd/c_sma1cl.f90
@@ -1,7 +1,22 @@
 !*==/home/marcusmae/nasa/nastran/SPAGged/C_SMA1CL.f90  created by SPAG 7.61RG at 01:00 on 21 Mar 2022
 MODULE C_SMA1CL
+!SMA1BD
+!
+!
+!     SMA1 VARIABLE CORE BOOKKEEPING PARAMETERS
+!
+!
+!     SMA1 PROGRAM CONTROL PARAMETERS
+!
+!
+!     ECPT COMMON BLOCK
+!
    REAL :: Dodet, Frowic, Tnrows
    REAL, DIMENSION(200) :: Dummy
    INTEGER :: Idetck, Iopt4, Jmax, K4ggsw, Left, Lrowic, Nlinks, Nogoo, Npvt, Nrowsc
    INTEGER, DIMENSION(10) :: Link
+!
+   DATA nogoo/0/
+   DATA nlinks/10/
+
 END MODULE C_SMA1CL
